558176ccb3bd48d671aa8fca0e0894d852b214bd
gasoul <gasoul@sujs.com>
2019-02-25 10:18:24 +0800

    增加了年龄和地址属性

--- gif_demo/src/main/java/cn/gasoul/model/User.java
+++ gif_demo/src/main/java/cn/gasoul/model/User.java
@@ -3,6 +3,8 @@ package cn.gasoul.model;
 public class User {
     private String username;
     private String password;
+    private Long age;
+    private String address;
 
     @Override
     public String toString() {
